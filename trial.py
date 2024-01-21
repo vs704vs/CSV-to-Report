e75a64324e0802517c1ade40facbc88d7544fef0
Vishal <vs704.vs@gmail.com>
2024-01-21 18:06:35 +0530

    changed txt to text. resolved the warning

--- trial.py
+++ trial.py
@@ -5,7 +5,7 @@ def create_pdf():
     pdf = FPDF()
     pdf.add_page()
     pdf.set_font("Arial", size=15)
-    pdf.cell(200, 10, txt="Hello World!", ln=1)
+    pdf.cell(200, 10, text="Hello World!", ln=1)
     pdf.output("output.pdf")
 
 create_pdf()
